e10f4c7c203f845386a1ff5f9e4cac51b8900782
DonRuan22 <ruan_donino@hotmail.com>
2021-07-02 15:46:21 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,149 @@
+from flask import Flask, request
+import requests
+import json
+import config
+
+
+
+app = Flask(__name__)
+SECRET_KEY = '51036c3d0c5ce54e47b39ce356c7af43'
+PAGE_ACCESS_TOKEN = 'EAA6NcE6FF4IBAHMbBCgcMxQIKCqHXLnJvdqt5DWSQCOoJJutTFbHYNLUPN0dDxLw6IlY3JrNZBoydgFQ5fbZBMjcAJbdUW0E3W7mqa5aJGoJv6ZA1bj9nvQnglkFZBYtpTRb4Sia31ICCqZB4UjGFqdPtAWpZA7lDZCw0sKynwxWQo2ZCSc31No9'
+VERIFY_TOKEN = 'rasa-don'
+
+
+
+#Function to access the Sender API
+def callSendAPI(senderPsid, response, type_response='message'):
+    #PAGE_ACCESS_TOKEN = config.PAGE_ACCESS_TOKEN
+    if(type_response == 'message'):
+        payload = {
+        'recipient': {'id': senderPsid},
+        'message': response,
+        'messaging_type': 'RESPONSE'
+        }
+    if(type_response == "sender_action"):
+        payload = {
+        'recipient': {'id': senderPsid},
+        "sender_action": "typing_on",
+        }    
+    headers = {'content-type': 'application/json'}
+
+    url = 'https://graph.facebook.com/v11.0/me/messages?access_token={}'.format(PAGE_ACCESS_TOKEN)
+    r = requests.post(url, json=payload, headers=headers)
+    print(r.text)
+
+
+
+#Function for handling a message from MESSENGER
+def handleMessage(senderPsid, receivedMessage):
+    callSendAPI(senderPsid, "","sender_action")
+    
+    #check if received message contains text
+    if 'text' in receivedMessage:
+        payload = {'sender': senderPsid,'message': receivedMessage['text']}
+        #payload_json = json.loads(payload)
+        print(payload)
+        response_rasa = requests.post('https://don-edml6m2f3a-uc.a.run.app/webhooks/rest/webhook', json = payload)
+        print(response_rasa.json()[0]["text"])
+        #response = {"text": 'You just sent: {}'.format(receivedMessage['text']) }
+        response = {"text": response_rasa.json()[0]["text"] }
+
+        callSendAPI(senderPsid, response)
+    else:
+        response = {"text": 'This chatbot only accepts text messages'}
+        callSendAPI(senderPsid, response)
+
+
+
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+
+    return 'HOME'
+
+@app.route('/webhooks/facebook/webhook', methods=["GET", "POST"])
+def index():
+    if request.method == 'GET':
+        #do something.....
+        #VERIFY_TOKEN = config.VERIFY_TOKEN
+
+        if 'hub.mode' in request.args:
+            mode = request.args.get('hub.mode')
+            print(mode)
+        if 'hub.verify_token' in request.args:
+            token = request.args.get('hub.verify_token')
+            print(token)
+        if 'hub.challenge' in request.args:
+            challenge = request.args.get('hub.challenge')
+            print(challenge)
+
+        if 'hub.mode' in request.args and 'hub.verify_token' in request.args:
+            mode = request.args.get('hub.mode')
+            token = request.args.get('hub.verify_token')
+
+            if mode == 'subscribe' and token == VERIFY_TOKEN:
+                print('WEBHOOK VERIFIED')
+
+                challenge = request.args.get('hub.challenge')
+
+                return challenge, 200
+            else:
+                return 'ERROR', 403
+
+        return 'SOMETHING', 200
+
+
+    if request.method == 'POST':
+        #do something.....
+        #VERIFY_TOKEN = config.VERIFY_TOKEN
+
+        if 'hub.mode' in request.args:
+            mode = request.args.get('hub.mode')
+            print(mode)
+        if 'hub.verify_token' in request.args:
+            token = request.args.get('hub.verify_token')
+            print(token)
+        if 'hub.challenge' in request.args:
+            challenge = request.args.get('hub.challenge')
+            print(challenge)
+
+        if 'hub.mode' in request.args and 'hub.verify_token' in request.args:
+            mode = request.args.get('hub.mode')
+            token = request.args.get('hub.verify_token')
+
+            if mode == 'subscribe' and token == VERIFY_TOKEN:
+                print('WEBHOOK VERIFIED')
+
+                challenge = request.args.get('hub.challenge')
+
+                return challenge, 200
+            else:
+                return 'ERROR', 403
+
+
+
+        #do something else
+        data = request.data
+        body = json.loads(data.decode('utf-8'))
+
+
+        if 'object' in body and body['object'] == 'page':
+            entries = body['entry']
+            for entry in entries:
+                webhookEvent = entry['messaging'][0]
+                print(webhookEvent)
+
+                senderPsid = webhookEvent['sender']['id']
+                print('Sender PSID: {}'.format(senderPsid))
+
+                if 'message' in webhookEvent:
+                    handleMessage(senderPsid, webhookEvent['message'])
+
+                return 'EVENT_RECEIVED', 200
+        else:
+            return 'ERROR', 404
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5005', debug=True)
