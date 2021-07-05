7f447243a91956b36c22d85fd51243dd5a2be2bd
DonRuan22 <ruan_donino@hotmail.com>
2021-07-05 09:00:38 -0300

    add cloud run files

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def callSendAPI(senderPsid, response, type_response='message'):
 
     url = 'https://graph.facebook.com/v11.0/me/messages?access_token={}'.format(PAGE_ACCESS_TOKEN)
     r = requests.post(url, json=payload, headers=headers)
-    logging.warning(r)
+    logging.warning(r.json())
     print(r.text)
 
 
