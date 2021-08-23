21734e84fef2d472c63e43c98d340ff72455a876
DonRuan22 <ruan_donino@hotmail.com>
2021-08-23 20:57:31 -0300

    add translator

--- app.py
+++ app.py
@@ -49,14 +49,14 @@ def handleMessage(senderPsid, receivedMessage):
     #check if received message contains text
     if 'text' in receivedMessage:
         translator_en = Translator()
-        receivedMessage_pt = translator_en(receivedMessage['text'], dest ='en')
+        receivedMessage_pt = translator_en.translate(receivedMessage['text'], dest ='en')
         receivedMessage['text'] = receivedMessage_pt.text
         payload = {'sender': senderPsid,'message': receivedMessage['text']}
         #payload_json = json.loads(payload)
         #print(payload)
         response_rasa = requests.post('https://don-edml6m2f3a-uc.a.run.app/webhooks/rest/webhook', json = payload)
         translator_pt = Translator()
-        response_port = translator_pt(response_rasa.json()[0]["text"], dest ='pt')
+        response_port = translator_pt.translate(response_rasa.json()[0]["text"], dest ='pt')
         #print(response_rasa.json()[0]["text"])
         #response = {"text": 'You just sent: {}'.format(receivedMessage['text']) }
         response = {"text": response_port.text}
